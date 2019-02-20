cde6152931e0bd8e8b9d86902fd9d91adaa41496
mech12 <whdnrfo@gmail.com>
2019-02-20 14:33:51 +0900

    start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,32 @@
-pm2 start server.js -i 1 --name 'olle'
+#!/bin/bash
+
+
+
+DIRECTORY=$(cd `dirname $0` && pwd)
+cd $DIRECTORY
+
+pushd ../env
+git reset --hard && git pull
+popd
+
+# export GOOGLE_APPLICATION_CREDENTIALS=/home/ec2-user/12ships/env/doc/google_12ships.json
+
+
+SERVICE_MODE=$(env | grep SERVICE_MODE | grep -oe '[^=]*$');
+echo "current SERVICE_MODE is $SERVICE_MODE"
+if [ $SERVICE_MODE = live ]
+then
+	source ../env/12ships.live.env
+    git checkout master
+else
+	source ../env/12ships.test.env
+    git checkout test
+fi
+
+git reset --hard && git pull
+npm -y install 
+npm run build
+pm2 delete monitor
+pm2 start server.js --name 'monitor'
+
 
